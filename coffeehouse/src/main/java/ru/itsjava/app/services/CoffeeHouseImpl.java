8250687e3f582891770f0736c2946f9d845f76ee
Petr Dankin <petr2768@yandex.ru>
2021-02-09 18:00:16 +0300

    заполнение коллекции в конструкторе

--- coffeehouse/src/main/java/ru/itsjava/app/services/CoffeeHouseImpl.java
+++ coffeehouse/src/main/java/ru/itsjava/app/services/CoffeeHouseImpl.java
@@ -2,24 +2,27 @@ package ru.itsjava.app.services;
 
 import ru.itsjava.dto.Coffee;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class CoffeeHouseImpl implements ICoffeeHouse{
+    private List<Coffee> coffees;
+    public CoffeeHouseImpl(){
+        this.coffees = Arrays.asList(
+            new Coffee("Lavazza", "espresso", 45.00),
+            new Coffee("Lavazza", "cappuchino", 50.10),
+            new Coffee("Lavazza", "americano", 60.25),
+            new Coffee("Illy", "espresso", 45.50),
+            new Coffee("Illy", "capuchino", 45.50),
+            new Coffee("Illy", "americano", 45.50),
+            new Coffee("Pauling", "espresso", 20.50),
+            new Coffee("Pauling", "capuchino", 40.30),
+            new Coffee("Pauling", "americano", 10.90)
+        );
+    }
     @Override
     public void buycoffee(Coffee coffee) {
-        List<Coffee> coffeeList = new ArrayList<>();
-        coffeeList.add(new Coffee("Lavazza", "espresso", 45.00));
-        coffeeList.add(new Coffee("Lavazza", "cappuchino", 50.10));
-        coffeeList.add(new Coffee("Lavazza", "americano", 60.25));
-        coffeeList.add(new Coffee("Illy", "espresso", 45.50));
-        coffeeList.add(new Coffee("Illy", "capuchino", 45.50));
-        coffeeList.add(new Coffee("Illy", "americano", 45.50));
-        coffeeList.add(new Coffee("Pauling", "espresso", 20.50));
-        coffeeList.add(new Coffee("Pauling", "capuchino", 40.30));
-        coffeeList.add(new Coffee("Pauling", "americano", 10.90));
-
-        for(Coffee c : coffeeList){
+        for(Coffee c : coffees){
             int number = c.compareTo(coffee);
             if(number == 0){
                 System.out.println("Получите ваше кофе: " + c.toString());
